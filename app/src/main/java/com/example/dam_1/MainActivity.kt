2e0e386a849ad2b28651b1660efa769b5626a46d
pablormacia <pr.macia@gmail.com>
2026-04-23 15:19:23 -0300

    Base

--- app/src/main/java/com/example/dam_1/MainActivity.kt
+++ app/src/main/java/com/example/dam_1/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.dam_1
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,11 +9,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-        val button: Button = findViewById<Button>(R.id.miBoton)
-
-        button.setOnClickListener{
-            Toast.makeText(this,"Has presionado el botón",Toast.LENGTH_SHORT).show()
-        }
     }
 }
